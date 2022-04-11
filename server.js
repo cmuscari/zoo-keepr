4056e261e90982710116105b15b2bbb966e556cc
Cortnie <cortniemuscari@gmail.com>
2022-04-11 16:20:44 -0500

    MVP finished

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ const app = express();
 
 
 
-
-
-
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -50,6 +47,20 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
+
+
+
+
+
+
+// ROUTES
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
 
@@ -57,6 +68,17 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+
+    if(result) {
+        res.json(result);
+    }
+    else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
