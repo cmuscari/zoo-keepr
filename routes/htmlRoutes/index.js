11cb1a4ae9ea1359646694f0683b027c57a6ca99
Cortnie <cortniemuscari@gmail.com>
2022-04-13 17:48:43 -0500

    separate data & zookeepers add

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,27 +1,20 @@
-const router = require('express').Router();
-const path = require('path');
+const path = require("path");
+const router = require("express").Router();
 
-
-
-// sets up the route to display the main html page in the browser
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
-// sets up the routes to display additional html pages in browser
-router.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/animals.html'));
+router.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/animals.html"));
 });
 
-router.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+router.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/zookeepers.html"));
 });
 
-// sets up a route to display main page if user makes a request for a route that doesn't exist
-router.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
-
-
 
 module.exports = router;
